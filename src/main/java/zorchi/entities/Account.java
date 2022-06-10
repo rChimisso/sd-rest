12cd1a110ff4a9915cb6e0de27949ad5bea47d28
Crystal Spider <riccardo.chimisso@gmail.com>
2022-06-11 00:42:39 +0200

    Definiti i metodi di gestione delle richieste e implementati alcuni. Definita la gestione degli UUID e degli Account.

--- src/main/java/zorchi/entities/Account.java
+++ src/main/java/zorchi/entities/Account.java
@@ -7,6 +7,8 @@ import org.springframework.lang.NonNull;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import zorchi.utility.StandardUUID;
+
 /**
  * Account bancario.
  */
@@ -16,28 +18,74 @@ public class Account {
    * Nome del proprietario dell'account.
    */
   @NonNull
-  private String name;
+  protected String name;
   /**
    * Cognome del proprietario dell'account.
    */
   @NonNull
-  private String surname;
+  protected String surname;
   /**
    * ID dell'account.
+   * <p>
+   * Composto da una sequenza di 20 caratteri esadecimali rappresentanti una sequenza di 10 byte (80 bit).
    */
   @Id
   @NonNull
   private final String ID;
 
   /**
-   * @param name - {@link #name nome}, passabile come proprietà {@code "name"} di un JSON.
-   * @param surname - {@link #surname cognome}, passabile come proprietà {@code "surname"} di un JSON.
+   * Costruttore senza argomenti per il funzionamento di Hibernate.
    */
-  public Account(@JsonProperty("name") String name, @JsonProperty("surname") String surname) {
-		this.name = name;
-		this.surname = surname;
-    this.ID = "";
+  public Account() {
+    this.ID = StandardUUID.INVALID_UUID;
+  }
+
+  /**
+   * @param name - {@link #name nome}.
+   * @param surname - {@link #surname cognome}.
+   * @param ID - {@link #ID}.
+   */
+  public Account(AccountData accountData, String ID) {
+    this.name = accountData.getName();
+    this.surname = accountData.getSurname();
+    this.ID = ID;
 	}
+
+  /**
+   * Restituisce il {@link #name nome} del proprietario dell'account.
+   * 
+   * @return {@link #name nome}.
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Restituisce il {@link #surname cognome} del proprietario dell'account.
+   * 
+   * @return {@link #surname cognome}.
+   */
+  public String getSurname() {
+    return surname;
+  }
+
+  /**
+   * Restituisce l'{@link #ID} dell'account.
+   * 
+   * @return {@link #ID}.
+   */
+  public String getID() {
+    return ID;
+  }
+
+  /**
+   * Controlla se questo account è valido.
+   * 
+   * @return risultato del controllo.
+   */
+  public boolean isValid() {
+    return !ID.equals(StandardUUID.INVALID_UUID);
+  }
 
   @Override
   public boolean equals(Object obj) {
@@ -57,5 +105,48 @@ public class Account {
   @Override
   public String toString() {
     return "Account [ID=" + ID + ", name=" + name + ", surname=" + surname + "]";
+  }
+
+  /**
+   * Dati per la creazione di un {@link zorchi.entities.Account Account bancario}.
+   */
+  public class AccountData {
+    /**
+     * Nome del proprietario dell'account.
+     */
+    @NonNull
+    private final String name;
+    /**
+     * Cognome del proprietario dell'account.
+     */
+    @NonNull
+    private final String surname;
+
+    /**
+     * @param name - {@link #name nome}, passabile come proprietà {@code "name"} di un JSON.
+     * @param surname - {@link #surname cognome}, passabile come proprietà {@code "surname"} di un JSON.
+     */
+    public AccountData(@JsonProperty("name") String name, @JsonProperty("surname") String surname) {
+      this.name = name;
+      this.surname = surname;
+    }
+
+    /**
+     * Restituisce il {@link #name nome}.
+     * 
+     * @return {@link #name}.
+     */
+    public String getName() {
+      return name;
+    }
+
+    /**
+     * Restituisce il {@link #surname cognome}.
+     * 
+     * @return {@link #surname}.
+     */
+    public String getSurname() {
+      return surname;
+    }
   }
 }
---
+++ src/main/java/zorchi/utility/StandardUUID.java
@@ -0,0 +1,71 @@
+package zorchi.utility;
+
+import java.util.Random;
+import java.util.UUID;
+import java.util.function.Predicate;
+
+/**
+ * Wrapper di utility per la generazione di {@link UUID} a 16 byte (128 bit, 32 hex).
+ */
+public class StandardUUID {
+  /**
+   * Valore di invalidità di un qualsiasi UUID.
+   */
+  public static final String INVALID_UUID = "-1";
+
+  /**
+   * Restituisce una stringa rappresentante un {@link UUID}, assicurandosi che non sia duplicato in base al {@link Predicate predicato} passato.
+   * 
+   * @param duplicated - {@link Predicate} per il controllo della duplicazione.
+   * @return {@link UUID} sottoforma di stringa.
+   */
+  public static String randomUUID(Predicate<String> duplicated) {
+    String standardUUID = UUID.randomUUID().toString().replace("-", "");
+    while (duplicated.test(standardUUID)) {
+      standardUUID = UUID.randomUUID().toString().replace("-", "");
+    }
+    return standardUUID;
+  }
+
+  /**
+   * Wrapper di utility per la generazione di UUID a 10 byte (80 bit, 20 hex).
+   */
+  public class ShortUUID {
+    /**
+     * Lunghezza in byte di uno ShortUUID.
+     */
+    private static final int BYTES = 10;
+
+    /**
+     * Restituisce una stringa rappresentante uno ShortUUID, assicurandosi che non sia duplicato in base al {@link Predicate predicato} passato.
+     * 
+     * @param duplicated - {@link Predicate} per il controllo della duplicazione.
+     * @return ShortUUID sottoforma di stringa.
+     */
+    public static String randomShortUUID(Predicate<String> duplicated) {
+      String shortUUID;
+      do {
+        byte[] bytes = new byte[BYTES];
+        new Random().nextBytes(bytes);
+        shortUUID = byteToHex(bytes);
+      } while (duplicated.test(shortUUID));
+      return shortUUID;
+    }
+  
+    /**
+     * Converte una serie di byte in una stringa di caratteri esadecimali.
+     * 
+     * @param bytes - serie di byte da convertire.
+     * @return rappresentazione esadecimale dei byte passati.
+     */
+    private static String byteToHex(byte[] bytes) {
+      String hex = "";
+      for (byte byt : bytes) {
+        String string = String.format("%02X", byt);
+        hex = hex + string;
+      }
+      return hex;
+    }
+  }
+}
+
