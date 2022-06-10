bca309ea820e92830548a890f5613d7ca5911ef1
Crystal Spider <riccardo.chimisso@gmail.com>
2022-06-10 18:32:18 +0200

    Added JPA and H2 dependencies.

--- src/main/java/zorchi/rest/exeption/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package zorchi.rest.exeption;
-
-import org.springframework.web.bind.annotation.*;
-import org.springframework.http.*;
-
-@ResponseStatus(code = HttpStatus.BAD_REQUEST)
-public class BadRequestException extends RuntimeException {
-	private static final long serialVersionUID = -5724630149535766317L;
-}
