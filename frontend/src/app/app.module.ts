86538251f38ad8438f65a7506cedd7d940bafa23
Crystal Spider <riccardo.chimisso@gmail.com>
2022-06-17 21:24:50 +0200

    Aggiunta formattazione currency.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
+import {registerLocaleData} from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
-import {NgModule} from '@angular/core';
+import localeIt from '@angular/common/locales/it';
+import {DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {EffectsModule} from '@ngrx/effects';
 import {StoreModule} from '@ngrx/store';
@@ -8,6 +10,8 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ApiService} from './core/services/api-service.service';
 import {RootModule} from './root/root.module';
+
+registerLocaleData(localeIt, 'it-IT');
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,7 +23,17 @@ import {RootModule} from './root/root.module';
     EffectsModule.forRoot([]),
     HttpClientModule
   ],
-  providers: [ApiService],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'it-IT'
+    },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'EUR'
+    },
+    ApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
