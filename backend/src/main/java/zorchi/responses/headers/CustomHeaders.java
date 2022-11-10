1cb9dfa7696b26d21dfa60e6e03604d2be803c4c
MauroZorzin <m.zorzin@campus.unimib.it>
2022-11-10 15:18:19 +0100

    Fixed design Code Smell src/main/java/zorchi/responses/headers/CustomHeaders.java line 13

--- backend/src/main/java/zorchi/responses/headers/CustomHeaders.java
+++ backend/src/main/java/zorchi/responses/headers/CustomHeaders.java
@@ -9,11 +9,18 @@ import org.springframework.util.LinkedMultiValueMap;
  * Utility per la generazione di header custom.
  */
 public abstract class CustomHeaders {
+  // Add a private constructor to hide the implicit public one.
+  private CustomHeaders() {
+    throw new IllegalStateException("Utility class");
+  }
+
   /**
    * Restituisce l'header "X-Sistema-Bancario".
    * 
-   * @param name - {@link zorchi.entities.Account#name nome} di un {@link zorchi.entities.Account Account bancario}.
-   * @param surname - {@link zorchi.entities.Account#surname cognome} di un {@link zorchi.entities.Account Account bancario}.
+   * @param name    - {@link zorchi.entities.Account#name nome} di un
+   *                {@link zorchi.entities.Account Account bancario}.
+   * @param surname - {@link zorchi.entities.Account#surname cognome} di un
+   *                {@link zorchi.entities.Account Account bancario}.
    * @return l'header "X-Sistema-Bancario".
    */
   public static LinkedMultiValueMap<String, String> getXSistemaBancarioHeader(String name, String surname) {
@@ -23,9 +30,10 @@ public abstract class CustomHeaders {
   /**
    * Genera un header data la {@code key} e il singolo {@code value}.
    * 
-   * @param key - chiave dell'header.
+   * @param key   - chiave dell'header.
    * @param value - valore dell'header.
-   * @return {@link LinkedMultiValueMap} per l'header di una {@link org.springframework.http.ResponseEntity ResponseEntity}.
+   * @return {@link LinkedMultiValueMap} per l'header di una
+   *         {@link org.springframework.http.ResponseEntity ResponseEntity}.
    */
   public static LinkedMultiValueMap<String, String> getCustomHeader(String key, String value) {
     return new LinkedMultiValueMap<>(Map.of(key, List.of(value)));
