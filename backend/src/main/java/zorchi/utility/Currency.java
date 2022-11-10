7e91b6bdbc2a5a4d9427acfbb66c13407e1ed4a2
MauroZorzin <m.zorzin@campus.unimib.it>
2022-11-10 15:16:21 +0100

    Fixed design Code Smell  src/main/java/zorchi/utility/Currency.java  line 9

--- backend/src/main/java/zorchi/utility/Currency.java
+++ backend/src/main/java/zorchi/utility/Currency.java
@@ -4,10 +4,16 @@ package zorchi.utility;
  * Utilità per gestire numeri che rappresentano currency.
  */
 public class Currency {
+  // Add a private constructor to hide the implicit public one.
+  private Currency() {
+    throw new IllegalStateException("Utility class");
+  }
+
   /**
    * Somma correttamente due valori monetari.
    * <p>
-   * Impedisce che la rappresentazione floating point dei due valori faccia comparire un errore durante la somma.
+   * Impedisce che la rappresentazione floating point dei due valori faccia
+   * comparire un errore durante la somma.
    * 
    * @param value1 - primo valore da sommare.
    * @param value2 - secondo valore da sommare.
@@ -20,7 +26,8 @@ public class Currency {
   /**
    * Sottrae correttamente due valori monetari.
    * <p>
-   * Impedisce che la rappresentazione floating point dei due valori faccia comparire un errore durante la sottrazione.
+   * Impedisce che la rappresentazione floating point dei due valori faccia
+   * comparire un errore durante la sottrazione.
    * 
    * @param value1 - primo valore da sottrarre.
    * @param value2 - secondo valore da sottrarre.
